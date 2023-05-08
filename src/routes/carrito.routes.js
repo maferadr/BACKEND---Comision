18cee5fda9a981c051a9a82e06c139e92513998a
maferadr <maferadr0604@gmail.com>
2023-05-08 09:39:51 -0400

    Pre-Entrega & websockets

--- src/routes/carrito.routes.js
+++ src/routes/carrito.routes.js
@@ -29,10 +29,12 @@ carritoRouter.post("/:cid/product/:pid", async (req, res)=>{
 
 carritoRouter.post("/carts", async (req, res)=>{
     const{id, products} = req.body
-    await cartManager.createCarrito((id, products) =>{
-        id = 1,
-        products = []
-    })
+    await cartManager.createCarrito({id, products}) //Llama a la funcion createCarrito ejecutada en CartManager.js
+
+    // await cartManager.createCarrito((id, products) =>{
+    //     id = 1,
+    //     products = []
+    // })
     res.send(`El producto con el ID ${req.params.id} se ha creado`)
 })
 
